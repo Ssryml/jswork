576e1f35e94aaa2cf5456f5eef2c58dc1dea8ea4
Ssryml <583191651@qq.com/>
2019-10-18 01:47:18 +0800

    work6

--- work6/pai.js
+++ work6/pai.js
@@ -1,32 +1,31 @@
 function calculate(){
-    let num = document.getElementById('pai').value
+    let num = document.getElementById('num').value
     num = parseInt(num)&&Number(num)
     if(!isNaN(num)){
-        document.getElementById('pai').value = pi(sum)
+        document.getElementById('pai').value=pi(num)
     }
 }
 function factorial(n){
     let sum = 1
-    for(let i =1;i<=n;++i){
-        
+    for(let i = 1;i<=n;++i){
+        sum *=i
     }
     return sum
 }
 function oddFactorial(n){
     let sum = 1
-    for(let i = 1;sum;++i){
-        
-    }
-    return sum
+    for(let i = 1;i<=n;++i){
+        sum *= 2*i+1
+}
+return sum
 }
 function pi(num){
-    let sum = 0
+    let sum =0
     for(let i=0;i<=num;++i){
-        let dividend = factorial( )
-        let divisor = oddFactorial( )
-        
-    }
-    return sum*2
+    let dividend =factorial(i)
+    let divisor = oddFactorial(i)
+    sum += (dividend/divisor)
 }
-calculate
-//未完成
+return sum * 2
+}
+calculate()
