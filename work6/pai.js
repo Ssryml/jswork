7ae77d6766c0c582dedf131aec04deb6976969f1
Ssryml <583191651@qq.com/>
2019-10-17 13:26:08 +0800

    work6

---
+++ work6/pai.js
@@ -0,0 +1,32 @@
+function calculate(){
+    let num = document.getElementById('pai').value
+    num = parseInt(num)&&Number(num)
+    if(!isNaN(num)){
+        document.getElementById('pai').value = pi(sum)
+    }
+}
+function factorial(n){
+    let sum = 1
+    for(let i =1;i<=n;++i){
+        
+    }
+    return sum
+}
+function oddFactorial(n){
+    let sum = 1
+    for(let i = 1;sum;++i){
+        
+    }
+    return sum
+}
+function pi(num){
+    let sum = 0
+    for(let i=0;i<=num;++i){
+        let dividend = factorial( )
+        let divisor = oddFactorial( )
+        
+    }
+    return sum*2
+}
+calculate
+//未完成
