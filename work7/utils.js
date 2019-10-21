8c6b3d2a6c3435a8ad9b4d63388d5d3643daf924
Ssryml <58319165@qq.com>
2019-10-21 11:30:05 +0800

    work7

--- work7/utils.js
+++ work7/utils.js
@@ -9,12 +9,12 @@ function bubbleSort(str){
     }
     return arr
 }
-function inserSort(str){
+function insertSort(str){
     let arr = str.split(',')
     for (let i = 1;i<arr.length; i++){
-        for(let j= i; j>0 ; j--){
-            if(arr[j-1]>arr[j]){
-                [arr[j-1],arr[j] = arr[j],arr[j-1]]
+        for(let j = i; j > 0 ; j--){
+            if(arr[ j - 1 ]>arr[ j ]){
+                [ arr[ j - 1 ],arr[ j ]] = [arr[ j ],arr[ j - 1 ] ]
             }
         }
     }
